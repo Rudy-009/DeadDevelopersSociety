b32f69c413f7cb3d92d15e119f62882c4330bc39
Miteum Park <133854554+wiiparks@users.noreply.github.com>
2023-06-26 21:17:45 +0900

    title added

--- OX/OX/ContentView.swift
+++ OX/OX/ContentView.swift
@@ -5,21 +5,40 @@
 //  Created by Aiden Park on 2023/06/26.
 //
 
+// 용우님! 30분까지 OX 마무리 해주세요 마감시간 지켜주세요.
+
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Button {
-                
-            } label: {
-                Text("오")
+            
+            Spacer()
+            
+            Text("맞춰봐")
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            HStack {
+                Button {
+                    
+                } label: {
+                    Text("오")
             }
-            Button {
-                
-            } label: {
-                Text("엑스")
+                    
+                Button {
+                    
+                } label: {
+                    Text("엑스")
+                }
             }
+            
+            
+            
+            Spacer()
+            
             }
         }
     }
