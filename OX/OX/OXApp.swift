f486c3cc7f0edaa1101064798c32b6228765b417
Miteum Park <133854554+wiiparks@users.noreply.github.com>
2023-06-26 20:54:31 +0900

    upload

---
+++ OX/OX/OXApp.swift
@@ -0,0 +1,17 @@
+//
+//  OXApp.swift
+//  OX
+//
+//  Created by Aiden Park on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct OXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
